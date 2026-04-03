bf8f4693710aef75bb5f58e4f2d347e78a6a9f9b
ariiagafonova <167536860+ariiagaf@users.noreply.github.com>
2026-04-04 00:47:05 +0200

    Revise comments in SceneDelegate.swift
    Updated project notes and copyright information in SceneDelegate.swift.

--- Magic 8 Ball/SceneDelegate.swift
+++ Magic 8 Ball/SceneDelegate.swift
@@ -2,10 +2,14 @@
 //  SceneDelegate.swift
 //  Magic 8 Ball
 //
-//📚 STUDENT PROJECT NOTES:
+//  Created by Angela Yu on 01/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+//  📚 STUDENT PROJECT NOTES:
 //  This is a learning project following the App Brewery's iOS course.
-//  Original story structure by Angela Yu. Implementation and extensions by Arina Agafonova
-//  Completed as part of iOS development learning journey - February-Mart 2026
+//  Original design and course materials belong to The App Brewery (Angela Yu).
+//  Code implementation and Auto Layout practice by Arina Agafonova.
+//  Completed as part of iOS development learning journey - February-March 2026.
 
 import UIKit
 
